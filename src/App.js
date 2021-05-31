c0401f42cf2e0e7dc9f175939dd2ef198d254b08
yungukbae <yungukbae@gmail.com>
2021-05-31 15:55:06 +0900

    setting new project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 
-import Navbar from './Navbar'
-import Home from './Home'
-import Create from './Create'
-import NotFound from './NotFound'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import BlogDetails from './BlogDetails';
-
-function App() {
+const App = () => {
 
   return (
-    <Router>
+    
     <div className="App">
-    <Navbar/>
-    <div className="content">
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-          </Route>
-          <Route path="/create">
-            <Create/>
-          </Route>
-          <Route path="/blogs/:id">
-            <BlogDetails/>
-          </Route>
-          <Route path="*">
-            <NotFound/>
-          </Route>
-      </Switch>
-    </div>
+
       
     </div>
-    </Router>
+    
 
   );
 }
